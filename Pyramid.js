f3b786c76bad647d2e8067f80e25814a41d2e52e
amit369 <amitchaman369@gmail.com>
2023-02-28 19:54:56 +0530

    changes done

---
+++ Pyramid.js
@@ -0,0 +1,15 @@
+
+for(let i=0;i<5;i++)
+{
+    for(let j=0;j<i;j++)
+    {
+        console.log(j);
+    }
+    console.log("");
+}
+
+var emp = {
+    name : "Daniel",
+    age : 23
+}
+console.log(emp);
